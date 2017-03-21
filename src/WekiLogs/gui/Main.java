a64a78b6c8ebf672136ae3ad647c3db337af4093
jeremie-garcia <jeremie.garcia@enac.fr>
2017-03-21 17:25:15 +0100

    initial commit to javaFX branch

---
+++ src/WekiLogs/gui/Main.java
@@ -0,0 +1,26 @@
+package WekiLogs.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This is the main class for the GUI
+ * It opens a folder by default and displays the content of the first file.
+ * @author jeremiegarcia
+ *
+ */
+public class Main extends Application {{
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
