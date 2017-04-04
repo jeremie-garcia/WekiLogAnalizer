a1ab6c26fb1bb4f803780299b08abf0393db06d3
Jérémie Garcia <j.garcia@gold.ac.uk>
2017-04-04 21:00:56 +0200

    removed unused files

--- klogs/WekiLogConfiguration.java
+++
@@ -1,7 +0,0 @@
-package klogs;
-
-public class WekiLogConfiguration {
-
-	////////////// FILES AND FOLDERS PREFERENCES
-	public static final String DEFAULT_ZIP_FILE = "./zipFilesTest/CreatingClassifiers-Parts1-3/32377/assignment2.zip";
-}
